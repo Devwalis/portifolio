4b6f8b65c325e728a69cf324f3a4fd161a0e645d
Walisson oliveira <walissonoliveira1@outlook.com>
2025-06-19 13:46:55 -0300

    Organzação de arquivos e pastas

--- meu-portfolio/src/App.jsx
+++ meu-portfolio/src/App.jsx
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Contato from './components/Contato';
+import Habilidades from './components/Hebilidades';
+import Hero from './components/Hero';
+import Servicos from './components/Servicos';
+import Sobre from './components/Sobre';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Hero />
+      <Sobre />
+      <Habilidades />
+      <Servicos />
+      <Contato />
     </div>
-  );
+  )
+  
 }
 
 export default App;
---
+++ meu-portfolio/src/components/Contato.jsx
@@ -0,0 +1,24 @@
+import '.Hero.css';
+import { useState } from 'react';
+
+ function Hero(){
+    const [mostrarProjetos, setMostrarProjetos] = useState(false);
+
+
+    return(
+        <section className="hero"> 
+        <h1>Olá! Meu nome é</h1>
+        <h2>Walisson Oliveira</h2>
+
+        <p>
+         Desenvolvedor de software certificado técnico em desenvolvimento de sistemas. 
+         Desenvolvo web sites interativos e automatizados para diferentes áreas de negócio.
+        </p>
+        <button onClick={() => alert('Redirecionar para Sobre')}>Sobre mim</button>
+        <button onClick={() => setMostrarProjetos(!mostrarProjetos)}>Sobre mim</button>
+
+        {mostrarProjetos && <p></p>}
+
+        </section>
+    );
+}
